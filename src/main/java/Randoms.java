508fcc73395eed6a3e2bcd131ba8ace4a67e73f5
pustovetov <1@1.ru>
2023-06-05 13:03:57 +0700

    fix

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Randoms {
+import java.util.ArrayList;
+import java.util.Iterator; import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random = new Random();
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            private int current;
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                current = random.nextInt(max - min + 1) + min;
+                return current;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
 }
